e0af0900bd84e3c9ada2999de4821f34fc2fed36
Sorokina23 <realpolosa@gmail.com>
2024-10-08 13:18:17 +0300

    Add files via upload

---
+++ Dz_21_09.py
@@ -0,0 +1,5 @@
+first_name = "Course:Pyton"
+total_tasks = 12
+hours_spent = 1.5
+average_execution_time = 0.125
+print("Курс:Pyton",",","total_task",":12","hours_spent",":1.5",",","verage_execution_time",(1.5%12),".")
